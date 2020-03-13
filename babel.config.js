05758b00e32c72469d8ff2c17972ce8cff9e95d9
Efosa Oyegun <efosao@gmail.com>
2020-03-13 13:02:46 -0700

    added layout page and css framework

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react']
+  presets: [
+    [
+      'next/babel',
+      {
+        'preset-env': {},
+        'preset-react': {}
+      }
+    ]
+  ]
 }
